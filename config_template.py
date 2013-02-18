021965ddb799962705815fc9a0fd29004d7d10cd
Jacob Meacham <jmeacham@palantir.com>
2013-02-18 00:19:39 -0800

    Update config template.

--- config_template.py
+++ config_template.py
@@ -9,3 +9,14 @@ FLATPAGES_AUTO_RELOAD = DEBUG
 FLATPAGES_EXTENSION = '.md'
 
 ADMINS = [User("admin", "password", 1)]
+
+# Mail Settings
+MAIL_SERVER = "smtp.yourmail.com"
+MAIL_PORT = "25"
+MAIL_USE_TLS = False
+MAIL_USE_SSL = False
+MAIL_USERNAME = ""
+MAIL_PASSWORD = ""
+DEFAULT_MAIL_SENDER = "SBCA"
+
+basedir = os.path.abspath(os.path.dirname(__file__))
