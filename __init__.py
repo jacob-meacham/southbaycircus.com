b388c8204593d22c7714d7514c7c95d6b2e0602e
Jacob Meacham <jacob.e.meacham@gmail.com>
2013-04-05 00:12:25 -0700

    Fix silly mistake with variable name.

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 import os
-from config import basedir, MAIL_SERVER, MAIL_PORT, DEFAULT_MAIL_SENDER, MAIL_USERNAME, MAIL_PASSWORD, ADMINS
+from config import basedir, MAIL_SERVER, MAIL_PORT, DEFAULT_MAIL_SENDER, MAIL_USERNAME, MAIL_PASSWORD, MAIL_ADMINS
 from flask import Flask
 from flask_flatpages import FlatPages
 from flask.ext.login import LoginManager
@@ -21,7 +21,7 @@ if not app.debug:
     credentials = None
     if MAIL_USERNAME or MAIL_PASSWORD:
         credentials = (MAIL_USERNAME, MAIL_PASSWORD)
-    mail_handler = SMTPHandler((MAIL_SERVER, MAIL_PORT), DEFAULT_MAIL_SENDER, ADMINS, 'SBCA.com Failure!', credentials)
+    mail_handler = SMTPHandler((MAIL_SERVER, MAIL_PORT), DEFAULT_MAIL_SENDER, MAIL_ADMINS, 'SBCA.com Failure!', credentials)
     mail_handler.setLevel(logging.ERROR)
     app.logger.addHandler(mail_handler)
 
