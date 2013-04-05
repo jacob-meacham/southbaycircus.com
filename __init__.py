853d79ef8e3d675b2ee694efcbe14588443d5f98
Jacob Meacham <jacob.e.meacham@gmail.com>
2013-04-05 00:07:50 -0700

    Add logging to file and error emails to admins.

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 import os
-from config import basedir
+from config import basedir, MAIL_SERVER, MAIL_PORT, DEFAULT_MAIL_SENDER, MAIL_USERNAME, MAIL_PASSWORD, ADMINS
 from flask import Flask
 from flask_flatpages import FlatPages
 from flask.ext.login import LoginManager
@@ -14,5 +14,25 @@ login_manager.init_app(app)
 login_manager.login_view = 'login'
 
 mail = Mail(app)
+
+if not app.debug:
+    import logging
+    from logging.handlers import SMTPHandler
+    credentials = None
+    if MAIL_USERNAME or MAIL_PASSWORD:
+        credentials = (MAIL_USERNAME, MAIL_PASSWORD)
+    mail_handler = SMTPHandler((MAIL_SERVER, MAIL_PORT), DEFAULT_MAIL_SENDER, ADMINS, 'SBCA.com Failure!', credentials)
+    mail_handler.setLevel(logging.ERROR)
+    app.logger.addHandler(mail_handler)
+
+if not app.debug:
+    import logging
+    from logging.handlers import RotatingFileHandler
+    file_handler = RotatingFileHandler('tmp/sbca.log', 'a', 1 * 1024 * 1024, 10)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    app.logger.setLevel(logging.INFO)
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+    app.logger.info('sbca startup')
 
 from app import views
