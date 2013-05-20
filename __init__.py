d4cb853d09617ef04d1ef113946796a2644c8f0e
Jacob Meacham <jacob.e.meacham@gmail.com>
2013-05-19 23:57:06 -0700

    Always log.

--- __init__.py
+++ __init__.py
@@ -25,14 +25,13 @@ if not app.debug:
     mail_handler.setLevel(logging.ERROR)
     app.logger.addHandler(mail_handler)
 
-if not app.debug:
-    import logging
-    from logging.handlers import RotatingFileHandler
-    file_handler = RotatingFileHandler(os.path.join(basedir, 'logs/sbca.log'), 'a', 1 * 1024 * 1024, 10)
-    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
-    app.logger.setLevel(logging.INFO)
-    file_handler.setLevel(logging.INFO)
-    app.logger.addHandler(file_handler)
-    app.logger.info('sbca startup')
+import logging
+from logging.handlers import RotatingFileHandler
+file_handler = RotatingFileHandler(os.path.join(basedir, 'logs/sbca.log'), 'a', 1 * 1024 * 1024, 10)
+file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+app.logger.setLevel(logging.INFO)
+file_handler.setLevel(logging.INFO)
+app.logger.addHandler(file_handler)
+app.logger.info('sbca startup')
 
 from app import views
